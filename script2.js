050a29eb5606dbbedb44e132f5e9ae1d5fd26220
rheshav <rhesa.havilah@gmail.com>
2022-02-20 18:38:29 +0700

    add files

---
+++ script2.js
@@ -0,0 +1,93 @@
+//get keyword
+const searchButton = document.querySelector('.search-button');
+searchButton.addEventListener('click', async function () {
+  try {
+    const inputKeyword = document.querySelector('.input-keyword');
+    const movies = await getMovies(inputKeyword.value);
+    updateUI(movies);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//event binding
+document.addEventListener('click', async function (e) {
+  if (e.target.classList.contains('modal-detail-button')) {
+    try {
+      const imdbid = e.target.dataset.imdbid;
+      const movieDetail = await getMoviesDetail(imdbid);
+      updateUIDetail(movieDetail);
+    } catch (error) {
+      alert(error);
+    }
+  }
+});
+
+function getMovies(keyword) {
+  return fetch('http://www.omdbapi.com/?apikey=4369a166&s=' + keyword)
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error(res.statusText);
+      }
+      return res.json();
+    })
+    .then((res) => {
+      if (res.Response === 'False') {
+        throw new Error(res.Error);
+      }
+      return res.Search;
+    });
+}
+
+function updateUI(movies) {
+  let cards = '';
+  movies.forEach((m) => (cards += showCards(m)));
+  const movieContainer = document.querySelector('.movie-container');
+  movieContainer.innerHTML = cards;
+}
+
+function getMoviesDetail(imdbid) {
+  return fetch('http://www.omdbapi.com/?apikey=4369a166&i=' + imdbid).then((res) => console.log(res));
+  // .then((response) => response.json())
+  // .then((m) => m);
+}
+
+function updateUIDetail(m) {
+  const movieDetail = showMovieDetail(m);
+  const modalBody = document.querySelector('.modal-body');
+  modalBody.innerHTML = movieDetail;
+}
+
+function showCards(m) {
+  return `<div class="col-md-4 my-3">
+    <div class="card">
+      <div class="card-body">
+      <img src="${m.Poster}" class="card-img-top"> 
+        <h5 class="card-title">${m.Title}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
+        <a href="#" class="btn btn-primary modal-detail-button" data-bs-toggle="modal" data-bs-target="#moviedetailModal" data-imdbid="${m.imdbID}">
+          Show Details
+        </a>
+      </div>
+    </div>
+  </div>`;
+}
+
+function showMovieDetail(m) {
+  return `<div class="container-fluid">
+    <div class="row">
+      <div class="col-md-3">
+        <img src="${m.Poster}" class="img-fluid" alt="" />
+      </div>
+      <div class="col-md">
+        <ul class="list-group">
+          <li class="list-group-item"><h4>${m.Title} (${m.Year})</h4></li>
+          <li class="list-group-item"><strong>Director :</strong> ${m.Director}</li>
+          <li class="list-group-item"><strong>Actors :</strong> ${m.Actors}</li>
+          <li class="list-group-item"><strong>Writer :</strong> ${m.Writer}</li>
+          <li class="list-group-item"><strong>Plot :</strong><br />${m.Plot}</li>
+        </ul>
+      </div>
+    </div>
+  </div>`;
+}
